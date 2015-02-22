abf8ebed5d0b71d13c77237c6355f6706c5f390a
Tom Kumschier <dev.angelicornis@icloud.com>
2015-02-22 20:36:45 +0100

    Initial Commit

---
+++ Kleiner Taschenrechner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kleiner Taschenrechner
+//
+//  Created by Tom Kumschier on 22.02.15.
+//  Copyright (c) 2015 Tom Kumschier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
