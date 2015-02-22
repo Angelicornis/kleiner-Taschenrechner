2d24adc7f9852f2690044351b005f6a4125ce7e6
Tom Kumschier <dev.angelicornis@icloud.com>
2015-02-22 21:19:51 +0100

    Berechnung hinzugefügt

--- Kleiner Taschenrechner/ViewController.swift
+++ Kleiner Taschenrechner/ViewController.swift
@@ -20,6 +20,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var zahl1TF: UITextField!
+    @IBOutlet weak var zahl2TF: UITextField!
+    @IBOutlet weak var operatorControl: UISegmentedControl!
+    
+    @IBOutlet weak var ergebnisLB: UILabel!
 
+    @IBAction func startButton() {
+        
+        var zahl1 = 0.0
+        var zahl2 = 0.0
+        //var zahl1 = (String(zahl1TF.text).toInt())
+        if (String(zahl1TF.text).toInt()) != nil {
+             zahl1 = Double(String(zahl1TF.text).toInt()!)
+        }else {
+            ergebnisLB.text = "Fehler 1"
+            return
+        }
+        if (String(zahl2TF.text).toInt()) != nil {
+             zahl2 = Double(String(zahl2TF.text).toInt()!)
+        }else {
+            ergebnisLB.text = "Fehler 2"
+            return
+        }
+        ergebnisLB.text = "Zahl 1: \(zahl1); Zahl 2: \(zahl2)"
+   
+        switch operatorControl.selectedSegmentIndex {
+        case 0: ergebnisLB.text = "\(zahl1 + zahl2)"
+        case 1: ergebnisLB.text = "\(zahl1 - zahl2)"
+        case 2: ergebnisLB.text = "\(zahl1 * zahl2)"
+        case 3: ergebnisLB.text = "\(zahl1 / zahl2)"
+        default: ergebnisLB.text = "Fehler3"
+        }
+    }
 }
 
